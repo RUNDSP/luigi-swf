c012344f29994e37fd2e881ceaf917726a93b7c6
Mike Placentra <michael.placentraii@gmail.com>
2014-11-18 10:36:18 -0500

    start of auto-cw

---
+++ luigi_swf/cw.py
@@ -0,0 +1,18 @@
+import argparse
+
+
+def cw_update(tasks):
+	pass
+
+
+def cw_on(task):
+	pass
+
+
+def cw_off(task):
+	pass
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser('Control CloudWatch monitoring')
+	# TODO
